45311bda94f0ff6fd75d78028496dbf6215106e6
JuK14 <julialex1407@gmail.com>
2022-08-30 15:41:51 +0400

    Написана вторая программа

--- Program.cs
+++ Program.cs
@@ -19,3 +19,28 @@ else
     Console.WriteLine(number + "- Не является палиндромом");
 }
 }
+
+
+//Задача 2
+
+Console.WriteLine("Введите координаты точки A по оси абсцисс: ");
+double Ax = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки A по оси ординат: ");
+double Ay = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки А по оси аппликат: ");
+double Az = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B по оси абсцисс: ");
+double Bx = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B по оси ординат: ");
+double By = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B по оси аппликат: ");
+double Bz = double.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((Ax-Bx),2)+ Math.Pow((Ay-By),2)+ Math.Pow((Az-Bz),2));
+
+Console.WriteLine("Расстояние между точкой А и точкой B равно  " + Math.Round(result,2));
