4b069a8326d667ff228f5609dc0de073c517b2ad
JuK14 <julialex1407@gmail.com>
2022-08-30 11:23:36 +0400

    Написана первая программа

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 1
+
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+if (number.Length > 5 || number.Length < 4)
+{
+  Console.WriteLine("Не пятизначное число");
+  
+}
+else
+{
+if (number[0] == number[4] && number[1] == number[3])
+{
+    Console.WriteLine(number + "- Является палиндромом");
+}
+else
+{
+    Console.WriteLine(number + "- Не является палиндромом");
+}
+}
